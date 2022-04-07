048cf9765e16fae74e80cebe3526d48397f2a23d
fermatspace <thomasvellekoop@hotmail.com>
2022-04-07 11:59:54 +0200

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,50 @@
+module Main where
+
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+
+import           Prelude
+import           System.Environment
+
+import           Cardano.Api
+import qualified Plutus.V1.Ledger.Api as Plutus
+import           Data.Aeson (encode)
+import qualified Data.ByteString.Short as SBS
+import qualified Data.ByteString.Lazy  as LBS
+import qualified Data.ByteString as B
+import qualified Data.Text as T
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Numeric (showHex)
+import GHC.Generics
+import           Cardano.Api.Shelley   (PlutusScript (..), fromPlutusData)
+import           Codec.Serialise       (serialise)
+import           PlutusTx              (Data (..))
+import qualified PlutusTx
+import qualified Ledger
+
+import Utils
+
+--main :: IO (Either (FileError ()) ())
+--main = writeValidator "plutus/script.plutus" $ validator
+
+main :: IO ()
+main = do
+    print "Compiles :)"
+--    writeJSON "testnet/datum.json" testDatum
+--    writeJSON "testnet/redeemer.json" testRedeemer
+
